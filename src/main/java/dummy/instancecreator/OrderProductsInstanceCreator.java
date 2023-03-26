ec750c2ea74fc11f9e71deace8aba31f2700be12
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-03-26 09:15:08 +0530

    Create OrderProductsInstanceCreator.java

---
+++ src/main/java/dummy/instancecreator/OrderProductsInstanceCreator.java
@@ -0,0 +1,31 @@
+package dummy.instancecreator;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.InstanceCreator;
+
+import dummy.model.Product;
+
+@Component
+public class OrderProductsInstanceCreator implements InstanceCreator<List<Product>> {
+	
+	@Autowired
+	ProductInstanceCreator productcreator;
+	
+	List<Product> products = new ArrayList<Product>();
+
+	@Override
+	public List<Product> createInstance(Type type) { 
+		products.add(productcreator.createInstance(Product.class));
+		products.add(productcreator.createInstance(Product.class));
+		products.add(productcreator.createInstance(Product.class));
+		return products.stream().distinct().collect(Collectors.toList()); // this is just a demo,you can pull the database via repository.
+	}
+
+}
