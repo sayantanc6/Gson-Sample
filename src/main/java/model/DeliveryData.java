5e3197aa5337d2f968abca05029c22b0f26bd3b5
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-03-26 09:29:47 +0530

    Create DeliveryData.java

---
+++ src/main/java/model/DeliveryData.java
@@ -0,0 +1,20 @@
+package dummy.model;
+
+import org.springframework.stereotype.Component;
+
+import com.google.gson.annotations.SerializedName;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryData {
+
+	@SerializedName("numOfPackages")
+	private int numOfPackages;
+	
+}
