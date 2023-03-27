9c7cc430b28d5e06080600a80490e17063bf70e9
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-03-27 17:05:37 +0530

    Update and rename ProductListSer.java to ProductListSerDe.java

--- src/main/java/dummy/customserde/ProductListSerDe.java
+++ src/main/java/dummy/customserde/ProductListSerDe.java
@@ -1,11 +1,10 @@
 package dummy.customserde;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -17,17 +16,23 @@ import com.google.gson.reflect.TypeToken;
 
 import dummy.model.Product;
 
-public class ProductListSer implements JsonSerializer<List<Product>> {
-	
-	List<Product> products= new ArrayList<Product>();
+public class ProductListSerDe implements JsonSerializer<List<Product>>,JsonDeserializer<List<Product>> {
 	
 	@Override
 	public JsonElement serialize(List<Product> src, Type typeOfSrc, JsonSerializationContext context) {
-		JsonObject obj = new JsonObject();
+		JsonArray array = new JsonArray();
 		src.forEach(p -> {
-			obj.addProperty("name", p.getName());
-			obj.addProperty("price", p.getPrice());
+			JsonObject object = new JsonObject();
+			object.addProperty("name", p.getName());
+			object.addProperty("price", p.getPrice());
+			array.add(object); 
 		});
-		return obj;
+		return array;
+	}
+
+	@Override
+	public List<Product> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException {
+		return new Gson().fromJson(json.getAsJsonArray(), new TypeToken<List<Product>>() {}.getType());
 	}
 }
