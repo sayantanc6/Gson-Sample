c78bc726be7acdb2f22e3f5b59f05120f53c7042
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-03-27 17:15:21 +0530

    Update ProductInstanceCreator.java

--- src/main/java/dummy/instancecreator/ProductInstanceCreator.java
+++ src/main/java/dummy/instancecreator/ProductInstanceCreator.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Component;
 import com.google.gson.InstanceCreator;
 
 import dummy.model.Product;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 
+@ConditionalOnBean(OrderInstanceCreator.class)
 @Component
 public class ProductInstanceCreator implements InstanceCreator<Product> {
 
