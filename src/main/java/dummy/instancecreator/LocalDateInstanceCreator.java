ff670fcd511ab3b354cbc59a966bf5a8a65da7f7
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-03-27 17:14:12 +0530

    Update LocalDateInstanceCreator.java

--- src/main/java/dummy/instancecreator/LocalDateInstanceCreator.java
+++ src/main/java/dummy/instancecreator/LocalDateInstanceCreator.java
@@ -6,7 +6,9 @@ import java.time.LocalDate;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.InstanceCreator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 
+@ConditionalOnBean(OrderInstanceCreator.class)
 @Component
 public class LocalDateInstanceCreator implements InstanceCreator<LocalDate> {
 
